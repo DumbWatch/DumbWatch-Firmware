36149a18f2563fa542277a1bb189e87200403245
Jesse Buhagiar <jooster669@gmail.com>
2021-09-18 01:56:54 +1000

    Driver: Flesh out `Default_Handler`

--- drivers/irq_handler.h
+++ drivers/irq_handler.h
@@ -10,23 +10,35 @@ namespace NRF52
 
 class IRQHandler
 {
+    static constexpr size_t NUMBER_OF_IRQS = 128;
+    static IRQHandler* s_handlers[NUMBER_OF_IRQS];
+
 public:
     virtual ~IRQHandler()
     {
-        //uninstall_handler();
+        s_handlers[m_irq_number] = nullptr;
         NVIC_DisableIRQ(static_cast<IRQn_Type>(m_irq_number));
     }
 
-    virtual void handle_interrupt() = 0;
+    virtual void handle_irq() = 0;
 
 protected:
     IRQHandler(uint8_t irq_number) : m_irq_number(irq_number)
     {
-        //install_handler(irq_number, *this);
+        s_handlers[irq_number] = this;
         NVIC_EnableIRQ(static_cast<IRQn_Type>(m_irq_number));
     }
 
 private:
+    static void  Default_Handler()
+    {
+        auto irqn = SCB->ICSR & SCB_ICSR_VECTACTIVE_Msk;
+        auto* handler = s_handlers[irqn];
+        
+        if (handler != nullptr)
+            handler->handle_irq();
+    }
+
     uint8_t m_irq_number { 0 };
 };
 
