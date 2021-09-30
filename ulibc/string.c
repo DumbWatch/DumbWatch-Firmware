a3827708204115694026a704355ab791f84b58f5
Jesse Buhagiar <jooster669@gmail.com>
2021-09-30 23:14:15 +1000

    LibC: Add basic libc implementation
    We can build on this as we go.  We don't need too many libc
    functions, so it's better to implement these ourselves
    so we don't pull in the whole libc shipped with GCC.

---
+++ ulibc/string.c
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+#include <stdlib.h>
+
+
+char* strncpy(char* dest, const char* src, size_t num)
+{
+    char* cpy_ptr = dest;
+    while (*src != '\0')
+        *cpy_ptr++ = *src++;
+
+    for (; num > 0; num--)
+        *cpy_ptr++ = '\0';
+
+    return dest;
+}
