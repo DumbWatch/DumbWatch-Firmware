62783ca65b52c5a3d9d8220657a3fe1f024933c8
Jesse Buhagiar <jooster669@gmail.com>
2021-10-02 00:12:32 +1000

    LibC: Fix `strncpy` implementation

--- ulibc/string.c
+++ ulibc/string.c
@@ -4,12 +4,13 @@
 
 #include <stdlib.h>
 
-
 char* strncpy(char* dest, const char* src, size_t num)
 {
     char* cpy_ptr = dest;
-    while (*src != '\0')
+    while ((*src != '\0') && (num--))
+    {
         *cpy_ptr++ = *src++;
+    }
 
     for (; num > 0; num--)
         *cpy_ptr++ = '\0';
